f108269eb7aff7c3bbb482118b0abecabdd83c5a
Cyril Ledru <cyril.ledru@gmail.com>
2013-03-21 07:04:53 +0900

    Server reacts to changes on pathList
    Server becomes a Backbone Events and bind itself to the pathList.

--- app/js/server.js
+++ app/js/server.js
@@ -1,7 +1,13 @@
 define(['jquery'], function($) {
 	var Server = function(pathList) {
+		_.extend(this, Backbone.Events);
+
 		this.pathList = pathList;
+
+		this.listenTo(this.pathList, 'change', this.pushChanges());
 	};
+
+	_.extend()
 	
 	Server.prototype = {
 		startupSync: function() {
@@ -9,6 +15,10 @@ define(['jquery'], function($) {
 			$.getJSON("/gps-paths", function(data) {
 				self.pathList.reset(data);
 			});
+		},
+
+		pushChanges: function() {
+			console.log("changes occured");
 		}
 	};
 	
