8509ed85e0390c1fe72536444238e484828c542e
Cyril Ledru <cyril.ledru@gmail.com>
2013-04-14 18:06:34 -0400

    Skeleton for data collector
    Html, main app ui behavior, store prototype.

--- app/js/col/app.js
+++ app/js/col/app.js
@@ -1,10 +1,59 @@
 define([
-	'jquery'
+    'jquery',
+    'col/store'
 	],
 	
-	function($) {
+	function($, Store) {
 		var App = function() {
 			var self = this;
+            self.store = new Store();
+            
+            $('#stop').hide();
+            
+            if (self.store.isNew()) {
+                $('#start').attr("disabled", "disabled");
+            } else {
+                $('#event').val(self.store.getCurrentEvent());
+            }
+            
+            if (self.store.hasCurrentSession()) {
+                self.store.persistCurrentSession();
+            }
+            
+            if (self.store.isEmpty()) {
+                $('#push').attr("disabled", "disabled");
+            }
+            
+            $('change').click(function(event) {
+                var eventName = $('#event').val();
+                if (eventName) {
+                    self.store.currentEventChanged(eventName);
+                }
+            });
+            
+            $('start').click(function(event) {
+                $('start').hide();
+                $('stop').show();
+                $('#push').attr("disabled", "disabled");
+            });
+            
+            $('stop').click(function(event) {
+                $('start').show();
+                $('stop').hide();
+                $('#push').attr("disabled", "disabled");
+                
+                if (self.store.hasCurrentSession()) {
+                    self.store.persistCurrentSession();
+                }
+                
+                if (self.store.hasPersistedSessions()) {
+                    $('#push').removeAtter("disabled");
+                }
+            });
+            
+            $('push').click(function(event) {
+                $('#push').attr("disabled", "disabled");
+            });
 		};
 	
 		App.prototype = {
---
+++ app/js/col/store.js
@@ -0,0 +1,23 @@
+define([], function() {
+    var Store = function() {
+            
+    };
+    
+    Store.prototype.isNew = function () {
+        return true;
+    };
+    
+    Store.prototype.isEmpty = function () {
+        return true;
+    };
+    
+    Store.prototype.hasCurrentSession = function () {
+        
+    };
+    
+    Store.prototype.persistCurrentSession = function () {
+        
+    };
+    
+    return Store;
+});
